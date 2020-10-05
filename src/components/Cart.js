13e9e691875e54a3f1d6ac46e524da66c525a43d
MOHAMMED BEN HARRI <benharri0mohammed@gmail.com>
2020-10-05 15:09:02 +0100

    Shoppin Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -25,27 +25,27 @@ const Cart = () => {
     }
 
     const removeItem = (id) => {
-      const newItems = items.map (item => {
-        if (item.id !== id)
-          return item
-        return {}
-        })
-        setItems(newItems)
+      const newItems = items.filter(item => item.id !== id)
+      setItems(newItems)
     }
 
     const CartTotal = items.reduce((total, item) => (
         total+item.qty*item.price
     ),0).toFixed(2)
 
-    
+    const NoItems = <div className="no-items">No Items.</div>
+    const Items = items.map (item => (<CartItem key={item.id} removeItem={removeItem} updateQty={updateQty} {...item} />))
     return (
        <React.Fragment>
         <div className="cart-container">
+        <div className="cart-header">
          <h1 className="cart-title">Shopping Cart</h1>
+         <div className="items-counter">
+           {items.length}
+         </div>
+         </div>
          <div className="cart-content">
-            {
-                items.map (item => (<CartItem key={item.id} removeItem={removeItem} updateQty={updateQty} {...item} />))
-            }
+            {items.length > 0 ? Items : NoItems}
          </div>
          <div className="cart-total">
            <h1 disabled={(CartTotal === 0)} >Cart total: {CartTotal} $</h1>
